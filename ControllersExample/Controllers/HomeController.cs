1a6113d11061cd9119dad4121cb8a7e851460bbd
Feihuan Peng <feihuan.peng@spreetail.com>
2025-04-01 20:37:21 -0500

    asd

--- ControllersExample/Controllers/HomeController.cs
+++ ControllersExample/Controllers/HomeController.cs
@@ -3,14 +3,36 @@
 namespace ControllersExample.Controllers
 {
     // class has to be public
+    [Controller]// if this attribute added, dont have to surfix class with Controller
     public class Homecontroller: Controller
     {
         // this approach enable the method with the route attribute is called attribute routing
-        [Route("api/sayhello")] 
+        [Route("sayhello1")] 
+        [Route("sayhello2")] // all route attribute will fire this method!!
         public string method1()
         {
             return "Hello from method1";
 
         }
+
+        [Route("home")]
+        [Route("/")]
+        public string Index()
+        {
+            return "hello from index";
+        }
+        
+        [Route("about")]
+        public string About()
+        {
+            return "hello from about";
+        }
+        
+        [Route("contact-us/{mobile:regex(^\\d{{10}}$)}")] // need excape \ and {} , just double it!
+        public string Contact()
+        {
+            return "hello from contact";
+        }
+
     }
 }
